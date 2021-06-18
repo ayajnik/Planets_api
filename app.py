0f0dfb202d7713de6333b374b3e70678300f399d
Ayush Yajnik <ayush.yajnik@yahoo.com>
2021-06-18 12:31:45 -0400

    creating the app

---
+++ app.py
@@ -0,0 +1,16 @@
+##importing modules
+from flask import Flask, request,jsonify
+
+
+##creating an object to initialize the app
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
