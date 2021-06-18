f56c5451a6f039c8d165e650638f7b67201d8315
Ayush Yajnik <ayush.yajnik@yahoo.com>
2021-06-18 13:27:54 -0400

    ORM and schema ready

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ from flask import Flask, request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, Float, String
 import os
+from flask_marshmallow import Marshmallow
+
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-
 ##creating an object to initialize the app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"+os.path.join(basedir+"planets.db")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"+os.path.join(basedir,"planets.db")
 
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
 
 ##creating a route for home page
 @app.route("/home")
@@ -58,6 +61,42 @@ def parameters_modern(name:str,age:int):
 
 
 ##creating the ORM template for databases
+class Planet(db.Model):
+    __tablename__ = "planets"
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    distance = Column(Float)
+    radius = Column(Float)
+
+class Users(db.Model):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key = True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique = True)
+    password = Column(String)
+
+
+##creating the schema to retrieve data 
+class Userschema(ma.Schema):
+    class meta:
+        fields = ("id", "first_name", "last_name", "email", "password")
+
+class Planetschema(ma.Schema):
+    class meta:
+        fields = ("planet_id", "planet_name", "planet_type", "home_star", "mass", "distance", "radius")
+
+user_schema = Userschema()
+users_schema = Userschema(many=True)
+
+planet_schema = Planetschema()
+planets_schema = Planetschema(many=True)
+
+
+
 
 
 
